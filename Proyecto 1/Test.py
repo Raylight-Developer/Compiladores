f34f1ace2ede932e933637bb7960af2742ad33fb
Raylight-Developer <Raylight.creator@gmail.com>
2024-09-02 10:32:54 -0600

    Tests

--- Proyecto 1/Test.py
+++ Proyecto 1/Test.py
@@ -1,29 +1,100 @@
-# name = "samuel"
+from Logger import*
+from Editor import *
+from Test_Data import *
+from Semantic_Analyzer import *
+from SyntaxErrorListener import *
 
-# def nombre():
-#     name = "Samuel"
-#     print(name)
+class Tester(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle("Semantic Compiler")
 
-# print(name)
-# nombre()
+		self.code_output = Logger()
+		self.code_output.setPlaceholderText("Compiled code")
 
-# name = "sam"
-# num = 5
-# print(f"{type(name)}: {type(num)}")
+		self.log = Logger()
+		self.log.setPlaceholderText("Log")
 
-# if type(name) != type(num):
-#     print("no se puede")
+		self.table_functions : List[Symbol_Table] = []
+		self.table_variables : List[Symbol_Table] = []
+		self.table_classes   : List[Symbol_Table] = []
 
-# value = "palabra"
-# numero = value.split('"')
-# print(type(numero), numero)
-# # Lista de ejemplo
+		self.tabs = QTabWidget()
+		splitter = QSplitter()
+		splitter.addWidget(self.code_output)
+		splitter.addWidget(self.log)
+		self.tabs.addTab(splitter, "Main")
 
-# # Filtrar elementos no vacíos y unirlos en una sola cadena
-# result = ''.join(filter(None, numero))
+		main_layout = QVBoxLayout()
+		main_layout.addWidget(self.tabs)
+		widget = QWidget()
+		widget.setObjectName("Background")
+		widget.setLayout(main_layout)
 
-# print(result)  # Output: '5'
+		self.setCentralWidget(widget)
 
-# none = "1>=2"
-# value = none.split(">=")
-# print(value)
+		self.code = getCode()
+		self.succeses = 0
+		self.parse()
+
+	def parse(self):
+		for i, codigo in enumerate(self.code):
+			try:
+				resultado = self.compile(i, codigo)
+				self.log.append(G + f"Comiplation Succesful [{i}]" + RESET + "<br><br>")
+				self.code_output.append(f"{resultado}")
+				self.succeses += 1
+			except Exception as e:
+				self.log.append(R + f"Compilation Failed [{i}]" + RESET + "<br><br>")
+				self.code_output.append(str(e))
+		self.log.append(f"TESTS:  {len(self.code)}")
+		self.log.append(G + "PASSED: " + RESET + f"{self.succeses}")
+		self.log.append(R + "FAILED: " + RESET + f"{len(self.code) - self.succeses}")
+
+	def compile(self, i: int, code: str):
+		self.table_functions.append(Symbol_Table(self.log, "Fun"))
+		self.table_variables.append(Symbol_Table(self.log, "Var"))
+		self.table_classes  .append(Symbol_Table(self.log, "Cla"))
+
+		splitter = QSplitter()
+		splitter.addWidget(self.table_classes  [-1])
+		splitter.addWidget(self.table_functions[-1])
+		splitter.addWidget(self.table_variables[-1])
+		self.tabs.addTab(splitter, f"Test [{i}]")
+
+		self.log.append(f"Compiling [{i}]...")
+		self.log.append(TEST + f"<pre>{code}</pre>" + RESET)
+
+		try:
+			lexer = CompiscriptLexer(InputStream(code))
+			token_stream = CommonTokenStream(lexer)
+			parser = CompiscriptParser(token_stream)
+
+			error_listener = SyntaxErrorListener(self.log)
+			parser.removeErrorListeners()
+			parser.addErrorListener(error_listener)
+			
+			tree = parser.program()
+
+			if error_listener.has_error:
+				raise Exception("Error de sintaxis detectado durante la compilación.")
+
+			visitor = Semantic_Analyzer(self.log, self.table_functions[-1], self.table_variables[-1], self.table_classes[-1], parser)
+			visitor.visit(tree)
+
+			self.table_classes  [-1].resizeColumnsToContents()
+			self.table_functions[-1].resizeColumnsToContents()
+			self.table_variables[-1].resizeColumnsToContents()
+
+			return tree.toStringTree(recog=parser)
+
+		except Exception as e:
+			raise Exception(f"Compilation Error for [{i}] {e}")
+
+
+app = QApplication(sys.argv)
+font_id = QFontDatabase.addApplicationFont("./RobotoMono-Medium.ttf")
+app.setStyleSheet(open("./QStyleSheet.css", "r").read())
+Window = Tester()
+Window.showMaximized()
+app.exec()
---
+++ Proyecto 1/Test_Data.py
@@ -0,0 +1,23 @@
+def getCode():
+	return [
+"""
+var nombre ;
+var edad = 25;
+""",
+"""
+var globalVar = " soy global" ;
+fun miFuncion () {
+var localVar = " soy local" ;
+print globalVar ; // Acceso permitido
+print localVar ; // Acceso permitido
+}
+miFuncion () ;
+print globalVar ; // Acceso permitido
+//print localVar ; // Error : localVar no esta definida
+""",
+"""
+var miVariable;
+miVariable = " Ahora tengo un valor ";
+print miVariable ; // Imprime : Ahora tengo un valor
+""",
+	]
